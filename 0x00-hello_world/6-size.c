3971e6fd03d471740955cdb23dc66f315f668404
AxelBouvierM <you@example.com>
2022-02-03 09:06:50 -0800

    ad

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+printf("%ld", sizeof(char));
+printf("%ld", sizeof(int));
+printf("%ld", sizeof(float));
+printf("%ld", sizeof(double));
+return (0);
+}
