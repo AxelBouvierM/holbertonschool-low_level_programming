b89e6400b6103cc6625d2fa5b24cc1b641e69fff
AxelBouvierM <you@example.com>
2022-02-03 11:01:12 -0800

    not use printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Prints out a string to stdout.
  * Return: 0 if success.
  */
 int main(void)
 {
-char text [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n";
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", 59);
 return (1);
 }
