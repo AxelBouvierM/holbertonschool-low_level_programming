abb48407fdb5ba9162ed1f3c7c9c9318b55848b5
AxelBouvierM <you@example.com>
2022-02-03 10:40:35 -0800

    Not use printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
+return (1);
+}
