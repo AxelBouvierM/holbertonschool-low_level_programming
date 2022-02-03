5f41494580b56b5bdc40e3b447c31148a1669b48
AxelBouvierM <you@example.com>
2022-02-03 10:43:27 -0800

    Not use printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
+find("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
 return (1);
 }
