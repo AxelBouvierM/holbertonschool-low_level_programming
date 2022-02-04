56701e9546556a9b89c9995d0a1fcc1025003ab6
AxelBouvierM <you@example.com>
2022-02-04 10:16:05 -0800

    lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Prints out a string to stdout.
+ *Return: 0 if success.
+ */
+int main(void)
+{
+char ch;
+
+for (ch = 'z' ; ch <= 'a' ; ch++)
+{
+putchar(ch);
+}
+{
+putchar('\n');
+}
+return (0);
+}
