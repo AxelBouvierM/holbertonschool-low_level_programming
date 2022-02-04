1b1b865fa4be0f4c994a955182898ae6af07ba4c
AxelBouvierM <you@example.com>
2022-02-04 10:58:30 -0800

    assign a random number to the variable

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Prints out a string to stdout.
+ *Return: 0 if success.
+ */
+int main(void)
+{
+int c;
+for (c = 0; c <= 9 ; c++)
+{
+putchar(c % 10 + '0');
+if (c != 9)
+putchar(',');
+putchar(' ');
+}
+return (0);
+}
