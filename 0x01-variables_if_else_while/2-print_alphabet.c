ddb6318cb34e76d663c87da121c6cd1f7c47489e
AxelBouvierM <you@example.com>
2022-02-04 08:06:11 -0800

    prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Prints out a string to stdout.
+ *Return: 0 if success.
+ */
+int main(void)
+{
+char ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+return (0);
+}
