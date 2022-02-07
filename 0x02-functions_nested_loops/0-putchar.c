be0c07d22e7c3ce6adb091cb164e9cf0baa26d6c
AxelBouvierM <you@example.com>
2022-02-07 05:54:17 -0800

    prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,18 @@
-#include "_putchar"
+#include "main.h"
 /**
  *main - Prints out a string to stdout.
  *Return: 0 if success.
  */
 int main(void)
 {
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
