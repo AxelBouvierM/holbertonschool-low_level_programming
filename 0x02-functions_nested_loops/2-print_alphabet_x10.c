4691df4281aa99949a78193ffd79b6b5a2e72583
AxelBouvierM <you@example.com>
2022-02-07 07:20:28 -0800

    prints 10 times the alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,18 @@
 #include "main.h"
 /**
- *main - Prints out a string to stdout.
+ *print_alphabet_x10 - Prints out a string to 10.
  *Return: 0 if success.
  */
 void print_alphabet_x10(void)
 {
+char ch;
+int x;
+for (x = 0 ; x < 11 ; x++)
+{
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
+}
