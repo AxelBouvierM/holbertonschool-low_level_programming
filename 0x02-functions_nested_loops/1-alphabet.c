36acb6729cce3d01021f6b09373c5ce2879c3201
AxelBouvierM <you@example.com>
2022-02-07 06:32:08 -0800

    prints the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,13 @@
  *main - Prints out a string to stdout.
  *Return: 0 if success.
  */
-int main(void)
+void print_alphabet(void)
 {
 char ch;
 
-for (ch = 'a' ; ch = 'z' ; ch++)
+for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-putchar(ch);
+_putchar(ch);
 }
-putchar('\n');
-return (0);
+_putchar('\n');
 }
