69fa1f5a192714e7ec932a57e8357544cffcda8b
AxelBouvierM <you@example.com>
2022-02-07 06:53:25 -0800

    Print

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,12 @@
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+int i;
+char string[] = "_putchar";
+for (i = 0; i < 8; i++)
+{
+_putchar(string[i]);
+}
 _putchar('\n');
 return (0);
 }
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ *main - Prints out a string to stdout.
+ *Return: 0 if success.
+ */
+void print_alphabet_x10(void)
+{
