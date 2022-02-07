395bd8d90e3eb645602e562df3c0a244ea7b9e69
AxelBouvierM <you@example.com>
2022-02-07 11:05:22 -0800

    prints every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ *jack_bauer - Prints out a string to stdout.
+ *Return: 0 if success.
+ */
+void jack_bauer(void)
+{
+int h;
+int m;
+
+for (h = 0 ; h <= 23 ; h++)
+{
+for (m = 0 ; m <= 59 ; m++)
+{
+if (h < 10)
+{
+_putchar('0');
+}
+else
+{
+_putchar(h / 10 + '0');
+}
+_putchar(h % 10 + '0');
+if (m < 10)
+{
+_putchar('0');
+}
+else
+{
+_putchar(m / 10 + '0');
+}
+_putchar(m % 10 + '0');
+}
+}
+}
