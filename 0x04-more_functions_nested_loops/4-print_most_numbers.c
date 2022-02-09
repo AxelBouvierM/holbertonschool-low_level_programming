a7a9eedc754ba05f61e60f4f9d14cb58a6b2dd7f
AxelBouvierM <you@example.com>
2022-02-09 06:54:08 -0800

    prints the numbers whitout 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_most_numbers - Prints out a string to 10.
+ *Return: 0 if success.
+ */
+void print_most_numbers(void)
+{
+int n;
+for (n >= 0 ; n <= 9 ; n++)
+{
+if (!((n == 2) || (n == 4)))
+{
+_putchar(n);
+}
+}
+_putchar('\n');
+}
