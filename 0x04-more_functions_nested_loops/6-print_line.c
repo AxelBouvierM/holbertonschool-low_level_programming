cc577586cc185aed383be47dba3fe9d8f3ab97b1
AxelBouvierM <you@example.com>
2022-02-09 10:45:12 -0800

    draws a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,8 +1,22 @@
 #include "main.h"
 /**
- *print_alphabet_x10 - Prints out a string to 10.
- *Return: 0 if success.
+ *print_line - Prints out a string to 10.
+ *@n: First variable.
  */
 void print_line(int n)
 {
-  
+int v;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (v = 0; v < n; v++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
+
