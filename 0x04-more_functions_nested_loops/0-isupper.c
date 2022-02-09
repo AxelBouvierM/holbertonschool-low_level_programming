17b16a1182ec18328c8d53e611d76c74021a6b62
AxelBouvierM <you@example.com>
2022-02-09 06:13:23 -0800

    checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - returns the largest of 3 numbers
+ * @c: third integer
+ * Return: largest number
+ */
+
+int _isupper(int c)
+{
+if (c >= 65 ; c < 91 ; c++)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
