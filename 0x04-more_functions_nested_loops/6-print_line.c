2a3aa5a38635d105bfaa70fa3f891bb55227abb0
AxelBouvierM <you@example.com>
2022-02-09 09:55:51 -0800

    prints a square

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - Prints out a string to 10.
+ *Return: 0 if success.
+ */
+void print_line(int n)
+{
+  
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_square - Prints out a string to 10.
+ *@size: First variable
+ */
+void print_square(int size)
+{
+int l;
+int a;
+for (l = 1; l <= size; l++)
+{
+for (a = 1; a <= size; a++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
