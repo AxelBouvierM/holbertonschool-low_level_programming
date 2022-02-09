1466a4b4fa6f216da2fd299afbed812d547e9925
AxelBouvierM <you@example.com>
2022-02-09 06:34:43 -0800

    multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - returns the largest of 3 numbers
+ * @a: third integer
+ * @b: second integer
+ * Return: largest number
+ */
+
+int mul(int a, int b)
+{
+int mul;
+int result;
+
+mul = a * b;
+result = mul;
+return (result);
+}
