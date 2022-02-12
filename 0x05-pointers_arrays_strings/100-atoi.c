a43d3d925545d83c72cef8151c2197ca503cf638
AxelBouvierM <you@example.com>
2022-02-11 16:56:38 -0800

    convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,25 +1,59 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
- * _atoi - check the code
- * @s:  Pointers to src
- * Return: Retorno
+ * _atoi - converts string to an integer
+ * @s: string
+ *
+ * Return: an integer
  */
 int _atoi(char *s)
 {
-	int res = 0;
-	int i;
+	char c;
+	unsigned int i, n = 0, j = _strlen(s) - 1, null = -1;
+	int flag = 0, sign = 1;
 
-	for (i = 0; s[i] != '\0'; i++)
-{
-	if (s[i] > '9' || s[i] < '0')
-{
-	res = ((res * 10) + (s[i] - '0'));
-	return (res);
+	for (i = 0; i <= j; i++)
+	{
+		c = s[i];
+		if (c == '-')
+			sign *= -1;
+		if (c >= 48 && c <= 57)
+		{
+			n = n * 10 + (c - '0');
+			flag = 1;
+		}
+		if (flag == 1)
+		{
+			if (!(c >= 48 && c <= 57))
+				break;
+		}
+	}
+	if (sign == -1)
+	{
+		n *= -1;
+	}
+	if (j == null)
+		return (0);
+	return (n);
 }
-	else if (s[i] == ('0' && '9'))
+
+/**
+ * _strlen - string length
+ * @s: string
+ *
+ * Return: the length of a string
+ */
+int _strlen(char *s)
 {
-	return (0);
-}
-}
-	return (-1);
+	int i;
+	char a;
+
+	for (i = 0; i > -1 ; i++)
+	{
+		a = s[i];
+		if (a == '\0')
+			break;
+	}
+	return (i);
 }
