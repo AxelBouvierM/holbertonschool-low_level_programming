2e06819b86c6e52f3832bc0d9a6f2fa3163af2c3
AxelBouvierM <you@example.com>
2022-02-16 15:35:53 -0800

    adds two numbers

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * *infinite_add -  prints n elements of an array of integers
+ * @n1: Pointer.
+ * @n2: Second pointer.
+ * @r: Three pointer.
+ * @size_r: First variable..
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
