95cf00836a5813e6145f1ea701c607689ebd6910
AxelBouvierM <you@example.com>
2022-02-25 04:17:47 -0800

    prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * _pow_recursion - function the value of x raised to the power of y.
+ * @x: First variable.
+ * @y: Second function.
+ * Return: Return the value of x raised to the power of y
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return 0;
+}
