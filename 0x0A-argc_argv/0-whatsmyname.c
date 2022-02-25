f4de585c4cbd2935fa37027af80e78706446d8de
AxelBouvierM <you@example.com>
2022-02-25 07:07:41 -0800

    program that prints its name, followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: Pointer with the number of elements
  * Return: Return the value of x raised to the power of y
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
