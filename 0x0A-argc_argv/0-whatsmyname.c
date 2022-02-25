a73fb38b9a4bc7065b483396ae5c07c0ccb80a18
AxelBouvierM <you@example.com>
2022-02-25 04:10:41 -0800

    program that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * _pow_recursion - function the value of x raised to the power of y.
+ * @x: First variable.
+ * @y: Second function.
+ * Return: Return the value of x raised to the power of y
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
