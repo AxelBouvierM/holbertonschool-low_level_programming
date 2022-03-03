6e21438bfac42f001f06cd1aadb675c2a127d40c
AxelBouvierM <you@example.com>
2022-03-03 12:39:06 -0800

    function that reallocates a memory block

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _realloc - crea un array de ints
+ * @ptr: puntero
+ * @old_size: viejo size bytes
+ * @new_size: nuevo size bytes
+ * Return: puntero a posicion 0 0
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+void *p;
+p = ptr;
+
+if (ptr == NULL)
+{
+ptr = malloc(new_size);
+return (ptr);
+}
+
+if (new_size == old_size)
+{
+return (ptr);
+}
+
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+
+free(ptr);
+p = malloc(new_size);
+
+return (p);
+}
