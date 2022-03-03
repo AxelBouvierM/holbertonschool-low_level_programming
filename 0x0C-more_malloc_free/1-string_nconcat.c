aad2f6a41f528eb9074621adda1c07c368bf2115
AxelBouvierM <you@example.com>
2022-03-03 07:03:58 -0800

    readme

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - Multiplies two numbers.
+ * @s1: Pointer to a first string
+ * @s2: Pointer to a second string
+ * @n: Variable
+ * Return: Return
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i = 0;
+	int u = 0;
+	unsigned int z = 0;
+	int s;
+	char *s3;
+
+	if (s1 == NULL)
+	{
+		return ("");
+	}
+	if (s2 == NULL)
+	{
+		return ("");
+	}
+	while (s2[z] != '\0')
+	{
+		z++;
+		if (z >= n)
+		{
+			n = z;
+		}
+	}
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	s3 = malloc((s1 + 1 + n) * sizeof(char));
+	if (s3 == NULL)
+	{
+		return (NULL);
+	}
+	for (s = 0; s < i; s++)
+	{
+		if (s1[s] == '\0')
+			u = 1;
+		if (u == 0)
+			s3[s] = s1[s];
+		if (u == 1)
+		{
+			s3[s] = s2[z];
+			z++;
+		}
+	}
+	return (s3);
+}
