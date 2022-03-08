1d9ab16aa83b037e98d161ce2b05f4088bf23d60
AxelBouvierM <axelbouvierm@gmail.com>
2022-03-08 19:01:14 +0000

    create a new structure

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,22 +16,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (d == NULL)
 	{
 		return (NULL);
-		free(d);
 	}
 	d->name = _strdup(name);
 	if (d->name == NULL)
 	{
-		return (NULL);
-		free(d->name);
 		free(d);
+		return (NULL);
 	}
 	d->age = age;
 	d->owner = _strdup(owner);
 	if (d->owner == NULL)
 	{
-		return (NULL);
-		free(d->owner);
+		free(d->name);
 		free(d);
+		return (NULL);
 	}
 	return (d);
 	free(d);
