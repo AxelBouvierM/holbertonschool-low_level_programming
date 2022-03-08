9050835c1131f45f836423b26a413abd6e260d24
AxelBouvierM <axelbouvierm@gmail.com>
2022-03-08 19:07:13 +0000

    create a new structure

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+* _strdup - returns a pointer to a newly allocated space in memory.
+* @str: Size of string.
+* Return: Return string.
+*/
+char *_strdup(char *str)
+{
+	char *s;
+	int i = 0;
+	int n = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[n] != '\0')
+	{
+		n++;
+	}
+	s = malloc(n + 1 * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for ( ; i < n; i++)
+		s[i] = str[i];
+	return (s);
+}
+
+/**
  * *new_dog - Creates a new dog.
  * @name: Name of...
  * @age: Age of...
@@ -32,33 +61,4 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	return (d);
-}
-
-/**
-* _strdup - returns a pointer to a newly allocated space in memory.
-* @str: Size of string.
-* Return: Return string.
-*/
-char *_strdup(char *str)
-{
-	char *s;
-	int i = 0;
-	int n = 0;
-
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	while (str[n] != '\0')
-	{
-		n++;
-	}
-	s = malloc(n + 1 * sizeof(char));
-	if (s == NULL)
-	{
-		return (NULL);
-	}
-	for ( ; i < n; i++)
-		s[i] = str[i];
-	return (s);
 }
