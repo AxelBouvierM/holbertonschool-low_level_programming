4cb0c17dda374428c80fe6ad5bae6e4f625709be
AxelBouvierM <axelbouvierm@gmail.com>
2022-03-09 21:28:14 +0000

    test

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,6 @@
 #include "3-calc.h"
 #include <stdio.h>
-/*
+/**
  * main - Program that print the result of the operation
  * @argc: Length of argv
  * @argv: Pointer with the number of elements
@@ -21,5 +21,5 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 	printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
-	return (0\n);
+	return (0);
 }
