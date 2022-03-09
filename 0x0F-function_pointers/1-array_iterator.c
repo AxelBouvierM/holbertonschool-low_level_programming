19c6c267a2270001269cffe7aa6798e018cd7bcc
AxelBouvierM <axelbouvierm@gmail.com>
2022-03-09 15:24:55 +0000

    function that executes a function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 /**
  * array_iterator - Function that executes a function
@@ -10,7 +11,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int x;
 	unsigned int total;
-
+	
+	if (array == NULL)
+		return;
+	if (action == NULL)
+		return;
 	for (x = 0; x < size; x++)
 	{
 		total = array[x];
