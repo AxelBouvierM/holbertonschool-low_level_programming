6031fd8fa6d85072297e5007a4c4bf40a08420f3
AxelBouvierM <axelbouvierm@gmail.com>
2022-03-10 16:28:48 +0000

    function that prints numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,11 +10,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list args;
-	
+
 	va_start(args, n);
 	if (separator == NULL)
 	{
-		return;
+		separator = "";
 	}
 	for (i = 0; i < n; i++)
 	{
