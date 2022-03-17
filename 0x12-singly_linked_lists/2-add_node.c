4dc4a1ffd6d1c4296fefce7c9d6a0a6e611438ec
AxelBouvierM <axelbouvierm@gmail.com>
2022-03-17 15:42:31 +0000

    function that adds a new node at the beginning of a list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ *_strlen - Prints out a string to 10.
+ *@s: First variable
+ * Return: count
+ */
+int _strlen(const char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+{
+	count++;
+	s++;
+}
+	return (count);
+}
+
+/**
+ * add_node - Adds a new node at the beginning
+ * @head: First struct
+ * @str: CAMBIAR
+ * Return: The address of the new element or null if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	struct list_s *new_struct = malloc(sizeof(list_t));
+	
+	if (new_struct == NULL)
+	{
+		return (NULL);
+	}
+	new_struct->str = strdup(str);
+	new_struct->len = _strlen(str);
+	new_struct->next = *head;
+	*head = new_struct;
+
+	return (new_struct);
+}
