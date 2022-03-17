7d0e79c49382dc9897caeaf7219f60766d4fb2ab
AxelBouvierM <axelbouvierm@gmail.com>
2022-03-17 17:37:33 +0000

    Print before the main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print - Print before the main
+ */
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
