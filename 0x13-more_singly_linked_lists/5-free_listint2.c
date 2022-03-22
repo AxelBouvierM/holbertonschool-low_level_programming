2cad5e5d2c4551c1db7be1d712fe0be63de50fe6
AxelBouvierM <axelbouvierm@gmail.com>
2022-03-22 12:15:43 +0000

    function that frees a list and head = NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,11 +7,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
+	if (*head == NULL)
+	{
+		return;
+	}
 	while (*head != NULL)
 	{
 		tmp = *head;
 		*head = tmp->next;
 		free(tmp);
 	}
-	*head = NULL;
 }
