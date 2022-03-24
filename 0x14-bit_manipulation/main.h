57817099b83f80f5e3c45c5c99b3315240a1e12f
AxelBouvierM <axelbouvierm@gmail.com>
2022-03-24 16:12:58 +0000

    Prototypes

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,10 +1,9 @@
 #ifndef MAIN_H
-#define MIAN_H
+#define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
-
 #endif
