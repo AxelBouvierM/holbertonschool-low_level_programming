4e0e57d9b39cdb735a0a3a6f154c667fc439b387
AxelBouvierM <axelbouvierm@gmail.com>
2022-05-05 17:35:09 +0000

    function that inserts a new node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -45,5 +45,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->prev = tmp;
 	new_node->next = tmp->next;
 	tmp->next = new_node;
+	tmp->next->prev = new_node;
 	return (new_node);
 }
