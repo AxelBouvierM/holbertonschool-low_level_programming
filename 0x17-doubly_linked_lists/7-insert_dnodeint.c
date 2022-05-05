db4905ce918699f6ced0bd408b46ffc251263a13
AxelBouvierM <axelbouvierm@gmail.com>
2022-05-05 18:13:52 +0000

    function that inserts a new node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,29 +8,44 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = NULL;
-	dlistint_t *tmp = *h;
-	unsigned int i = 0;
+	dlistint_t *new_node;
+	dlistint_t *tmp;
+	unsigned int i;
 
+	tmp = *h;
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-	while (i != (idx - 1))
+	new_node->n = n;
+	while (tmp != NULL && tmp->prev != NULL)
 	{
-		if (tmp == NULL)
-			return (NULL);
+		tmp = tmp->prev;
+		*h = (*h)->prev;
+	}
+	if (idx == 0)
+	{
+		free(new_node);
+		return (add_dnodeint(h, n));
+	}
+	for (i = 0; i < (idx - 1) && tmp != NULL; i++)
 		tmp = tmp->next;
-		i++;
+	if (tmp	== NULL)
+	{
+		free(new_node);
+		return (NULL);
 	}
 	if (tmp->next == NULL)
-		return (add_dnodeint_end(h, n));
-	new_node->n = n;
-	new_node->next = tmp->next;
-	new_node->prev = tmp;
-	tmp->next = new_node;
-	tmp->next->prev = new_node;
-
+	{
+		new_node->next = NULL;
+		new_node->prev = tmp;
+		tmp->next = new_node;
+	} 
+	else
+	{
+		new_node->next = tmp->next;
+		new_node->prev = tmp;
+		tmp->next->prev = new_node;
+		tmp->next = new_node;
+	}
 	return (new_node);
 }
