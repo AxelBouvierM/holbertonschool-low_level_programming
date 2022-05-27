5f627eda4b746c899be15751ca4ecfc02ee11127
AxelBouvierM <axelbouvierm@gmail.com>
2022-05-27 15:05:15 +0000

    fixing task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,11 +1,13 @@
 #include "hash_tables.h"
 /**
+ * hash_table_delete - function that deletes a hash table
+ * @ht:is the hash table
  */
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_node_t *tmp, *aux;
 	unsigned long int count = 0;
-	
+
 	if (ht == NULL)
 		return;
 	while (count < ht->size)
